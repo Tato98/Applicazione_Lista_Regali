caa19d4899a1f5c9e2575e784790253e682bc57c
Corrado <corrado.raiola@gmail.com>
2020-03-31 13:09:04 +0200

    Inizio del Progetto. Creazione del Progetto e condivisione su Github.

---
+++ app/src/main/java/com/example/applicazione_lista_regali/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.applicazione_lista_regali;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
